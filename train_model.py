8ef9517994c53b6fa682f640b9752ac491c85f51
suryathe44 <suryasinghchauhan485@gmail.com>
2025-12-27 16:43:38 +0530

    Update train_model.py

--- train_model.py
+++ train_model.py
@@ -1,6 +1,6 @@
 import pandas as pd
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.naive_bayes import MultinomialNB
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.linear_model import LogisticRegression
 import joblib
 
 # Load dataset
@@ -9,16 +9,22 @@ data = pd.read_csv("dataset.csv")
 X = data["text"]
 y = data["label"]
 
-# Convert text to numbers
-vectorizer = CountVectorizer()
+# Vectorizer
+vectorizer = TfidfVectorizer(
+    ngram_range=(1,2),
+    stop_words="english",
+    max_features=5000
+)
+
 X_vec = vectorizer.fit_transform(X)
 
-# Train model
-model = MultinomialNB()
+# Model
+model = LogisticRegression()
 model.fit(X_vec, y)
 
-# Save model & vectorizer
+# Save model
 joblib.dump(model, "model.pkl")
 joblib.dump(vectorizer, "vectorizer.pkl")
 
-print("Model trained and saved successfully!")
+print("✅ Model trained and saved successfully")
+
